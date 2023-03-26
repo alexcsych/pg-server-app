bc4842e7cf8fe30cc1a77736f134e0b61d122dcb
Alexc <alexsejsi4@gmail.com>
2023-03-26 19:15:48 +0300

    Add yup validate

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const yup = require('yup');
 const { usersController } = require('../controllers');
 const { paginate } = require('../middleware');
 
@@ -7,7 +8,25 @@ const usersRouter = Router();
 
 usersRouter
   .route('/')
-  .post(usersController.createUser)
+  .post(async (req, res, next) => {
+    const { body } = req;
+    const NAME_VALIDATION_SCHEMA = yup.string().min(2).max(64).required();
+    const USER_CREATION_VALIDATION_SCHEMAS = yup.object({
+      firstName: NAME_VALIDATION_SCHEMA,
+      lastName: NAME_VALIDATION_SCHEMA,
+      email: yup.string().email(),
+      tel: yup
+        .string()
+        .length(13)
+        .matches(/^\+380\d{9}$/),
+    });
+    try {
+      req.body = await USER_CREATION_VALIDATION_SCHEMAS.validate(body);
+      next();
+    } catch (err) {
+      next(err);
+    }
+  }, usersController.createUser)
   .get(paginate.paginateUser, usersController.getUsers);
 
 usersRouter
